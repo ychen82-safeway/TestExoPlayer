02ff6d3677a8d1ffa5e94b7d89a88573d2c1a933
ychen82-safeway <121119477+ychen82-safeway@users.noreply.github.com>
2023-06-30 11:08:27 -0700

    use media3.exoplayer

--- app/src/main/java/com/example/myapplication/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/MainFragment.kt
@@ -1,31 +1,40 @@
 package com.example.myapplication.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import com.example.myapplication.R
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.ui.StyledPlayerView
 
 
-class MainFragment : Fragment(), Player.Listener {
+class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private var playWhenReady = true
     private lateinit var viewModel: MainViewModel
-    private lateinit var playerView: StyledPlayerView
+    private val playbackStateListener: Player.Listener = playbackStateListener()
+    private lateinit var playerView: PlayerView
+    private var playWhenReady = true
+    private var mediaItemIndex = 0
+    private var playbackPosition = 0L
+    private var player: Player? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-
     }
 
     override fun onCreateView(
@@ -37,44 +46,75 @@ class MainFragment : Fragment(), Player.Listener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        playerView = view.findViewById(R.id.exo_player_view)
+        playerView = view.findViewById(R.id.video_view)
     }
 
-    override fun onStart() {
-        super.onStart()
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
         initializePlayer()
+
     }
 
-    override fun onStop() {
-        super.onStop()
+    override fun onPause() {
+        super.onPause()
         releasePlayer()
     }
 
-
     private fun initializePlayer() {
-        viewModel.player = ExoPlayer.Builder(requireContext())
+        // ExoPlayer implements the Player interface
+        player = ExoPlayer.Builder(requireContext())
             .build()
-        playerView.player = viewModel.player
-        viewModel.player?.apply {
-            setMediaItem(viewModel.getMediaItem())
-            // prepare media list
-            playWhenReady = playWhenReady
-            seekTo(viewModel.currentWindow, viewModel.playbackPosition)
-            addListener(this@MainFragment)
-            prepare()
-        }
+            .also { exoPlayer ->
+                playerView.player = exoPlayer
+                // Update the track selection parameters to only pick standard definition tracks
+                exoPlayer.trackSelectionParameters = exoPlayer.trackSelectionParameters
+                    .buildUpon()
+                    .setMaxVideoSizeSd()
+                    .build()
 
+                val mediaItem = MediaItem.Builder()
+                    .setUri(getString(R.string.media_url_dash))
+                    .setMimeType(MimeTypes.APPLICATION_MPD)
+                    .build()
+                exoPlayer.setMediaItems(listOf(mediaItem), mediaItemIndex, playbackPosition)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.addListener(playbackStateListener)
+                exoPlayer.prepare()
+            }
+        playerView.setFullscreenButtonClickListener {  }
     }
 
     private fun releasePlayer() {
-        viewModel.player?.apply {
-            this@MainFragment.playWhenReady = playWhenReady
-            viewModel.playbackPosition = currentPosition
-            viewModel.currentWindow = currentMediaItemIndex
-            removeListener(this@MainFragment)
-            release()
+        player?.let { player ->
+            playbackPosition = player.currentPosition
+            mediaItemIndex = player.currentMediaItemIndex
+            playWhenReady = player.playWhenReady
+            player.removeListener(playbackStateListener)
+            player.release()
         }
-        viewModel.player = null
+        player = null
     }
 
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(requireActivity().window, false)
+        WindowInsetsControllerCompat(requireActivity().window, playerView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+}
+
+private fun playbackStateListener() = object : Player.Listener {
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        val stateString: String = when (playbackState) {
+            ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
+            ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
+            ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
+            ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
+            else -> "UNKNOWN_STATE             -"
+        }
+        Log.d("TAG", "changed state to $stateString")
+    }
 }
--- app/src/main/java/com/example/myapplication/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/main/MainViewModel.kt
@@ -1,24 +1,6 @@
 package com.example.myapplication.ui.main
 
-import android.net.Uri
 import androidx.lifecycle.ViewModel
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.util.MimeTypes
 
 class MainViewModel : ViewModel() {
-    private val videoURL =
-        "https://media.geeksforgeeks.org/wp-content/uploads/20201217163353/Screenrecorder-2020-12-17-16-32-03-350.mp4"
-
-    var currentWindow = 0
-    var playbackPosition: Long = 0
-    var player: ExoPlayer? = null
-    fun getMediaItem() : MediaItem {
-        // add one media item
-        // val mediaItem: MediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
-        return MediaItem.Builder()
-            .setUri(Uri.parse(videoURL))
-            .setMimeType(MimeTypes.BASE_TYPE_VIDEO)
-            .build()
-    }
 }
